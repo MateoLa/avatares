c7db532a21f39f12fddd933bc24fb7f586442779
MateoLa <mateo.laino@gmail.com>
2023-01-13 09:21:05 -0300

    second

--- lib/generators/avatares/install/templates/initializer.rb
+++ lib/generators/avatares/install/templates/initializer.rb
@@ -1,5 +1,11 @@
-Avatares.setup do |set|
-  set.color = "#FFFFFF"
-  set.size = "150x150"
-  set.font = "DejaVu-Sans"
+Avatares.setup do |config|
+  # Method to get the avatarable in the controller
+  # config.controller_avatarable = :current_user
+
+  # Default styles to be generated
+  # config.avatarable_styles = { small:  '50x50',
+  #                              medium: '120x120',
+  #                              large: '260x260' }
+
+  # config.font = "DejaVu-Sans"
 end
